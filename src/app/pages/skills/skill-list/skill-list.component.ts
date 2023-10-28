f60497037d979d222a79d4141c05ae290fdca6b1
Dowglas Maia <dowglasmaia@gmail.com>
2023-10-28 13:32:56 -0300

    impl listagem de skill de forma dinamica com o servidor

--- src/app/pages/skills/skill-list/skill-list.component.ts
+++ src/app/pages/skills/skill-list/skill-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SkillService } from '../services/skill.service';
+import { Skill } from '../model/skill.model';
 
 @Component({
   selector: 'app-skill-list',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private skillService: SkillService) { }
+
+  skills: Skill[] = [];
 
   ngOnInit() {
+
+    this.getAll()
+
+  }
+
+  private getAll() {
+    this.skillService.getAll()
+      .subscribe(
+        response => this.skills = response,
+        error => alert('Error ao carregar a lista de Skills')
+      )
+  }
+
+  private delete() {
+    alert('seu perfil pnão tem ermissão para delete uma Skill !!!')
   }
 
 }
